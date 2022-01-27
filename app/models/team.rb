52e8622bc1f93ea44cd82d284b6b5a0169d48730
Lucas Diniz Cunha <lucasdinizcunha2000@gmail.com>
2022-01-27 19:49:56 -0300

    delete hotfix

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,9 +5,9 @@ class Team < ApplicationRecord
     has_many :scrims, through: :team_scrims
     has_many :messages, dependent: :destroy
     has_many :evaluations, dependent: :destroy
-    has_many :chat_room_teams, dependent: :destroy
-    has_many :chat_rooms, through: :chat_room_teams
+    has_many :chat_rooms, dependent: :destroy
     has_many :invites, through: :invite_teams
     has_many :invite_teams, dependent: :destroy
     has_many :complaints, dependent: :destroy
+    has_many :messages, through: :chat_rooms, dependent: :destroy
 end
